a5987d7ac22a732b0c0f942ebde79b61ac7c5036
JIsaacVM <jivmzacisaac@gmail.com>
2025-05-16 14:28:07 -0600

    actualizacion v1 16/05/2025

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,17 @@
 // @ts-check
 import { defineConfig, envField } from 'astro/config';
-
 import react from '@astrojs/react';
+import astroI18next from "astro-i18next";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  integrations: [
+    react(),
+    astroI18next()],
 
-  env:{
-    schema:{
-      SCORE_API: envField.string({context:'server',access:'public'})
+  env: {
+    schema: {
+      SCORE_API: envField.string({ context: 'server', access: 'public' })
     }
   }
 });
--- src/data/infoproyecto.js
+++ src/data/infoproyecto.js
@@ -1,55 +1,60 @@
-    export const infoproyecto = [
-            {
-                nombre: "AppMedik",
-                objetivo: "Desarrollar una aplicación móvil pediátrica para la gestión de citas médicas y la dosificación de medicamentos.",
-                herramientas: [
-                    { nombre: "IDE", herramienta: "Android Studio 2023.1.13" },
-                    { nombre: "Framework", herramienta: "Node.js v.18.16.0" },
-                    { nombre: "Base de Datos", herramienta: "MongoDB Atlas v.4.4.6" },
-                    { nombre: "Servidor", herramienta: "AWS EC2 v.2.0.1" }
-                ],
-                resultados: "Aplicación Beta",
-                link: ""
-            },
-            {
-                nombre: "API CRUD para Restaurante “El Zarape”",
-                objetivo: "Crear servicios RESTful para operaciones CRUD en el sistema de punto de venta del restaurante.",
-                herramientas: [
-                    { nombre: "Base de Datos", herramienta: "MySQL 8.0.38" },
-                    { nombre: "IDE", herramienta: "Apache NetBeans IDE 24" },
-                    { nombre: "Servidor", herramienta: "Apache TomCat 10.1.13" },
-                    { nombre: "Cliente API", herramienta: "Postman v.11.31.4" },
-                    { nombre: "IDE Alternativa", herramienta: "IntelliJIDEA 2024.2.5" }
-                ],
-                resultados: "API REST para gestionar datos en una base de datos."
-            },
-            {
-                nombre: "Sistema DAQ para Monitoreo de Sensores",
-                objetivo: "Implementar un sistema que interactúe con sensores de temperatura y humedad, almacenando datos en una base de datos mediante una interfaz gráfica.",
-                herramientas: [
-                    { nombre: "Microcontrolador", herramienta: "Arduino Uno" },
-                    { nombre: "IDE", herramienta: "Arduino IDE 2.3.4" },
-                    { nombre: "IDE", herramienta: "Apache NetBeans IDE 24" },
-                    { nombre: "Servicio IoT", herramienta: "Dweet.io" }
-                ],
-                resultados: "Monitoreo en tiempo real con recolección de datos."
-            },
-            {
-                nombre: "Formulario de Usuarios en Android (CRUD)",
-                objetivo: "Implementar un sistema CRUD para gestionar usuarios en dispositivos móviles.",
-                herramientas: [
-                    { nombre: "IDE", herramienta: "Android Studio 2023.1.13" },
-                    { nombre: "Base de Datos", herramienta: "SQLite 2.4.0" }
-                ],
-                resultados: "Aplicación móvil funcional con base de datos integrada."
-            },{
-                nombre: "Formulario de Usuarios en Android (CRUD)",
-                objetivo: "Implementar un sistema CRUD para gestionar usuarios en dispositivos móviles.",
-                herramientas: [
-                    { nombre: "IDE", herramienta: "Android Studio 2023.1.13" },
-                    { nombre: "Base de Datos", herramienta: "SQLite 2.4.0" }
-                ],
-                resultados: "Aplicación móvil funcional con base de datos integrada."
-            }
+export const infoproyecto = [
+    {
+        nombre: "AppMedik",
+        img: "/img/appmedik.png",
+        objetivo: "Desarrollar una aplicación móvil pediátrica para la gestión de citas médicas y la dosificación de medicamentos.",
+        herramientas: [
+            { nombre: "IDE", herramienta: "Android Studio 2023.1.13" },
+            { nombre: "Framework", herramienta: "Node.js v.18.16.0" },
+            { nombre: "Base de Datos", herramienta: "MongoDB Atlas v.4.4.6" },
+            { nombre: "Servidor", herramienta: "AWS EC2 v.2.0.1" }
+        ],
+        resultados: "Aplicación Beta",
+        page: "appmedik"
+    },
+    {
+        nombre: "API CRUD para Restaurante “El Zarape”",
+        objetivo: "Crear servicios RESTful para operaciones CRUD en el sistema de punto de venta del restaurante.",
+        img: "/img/appmedik.png",
+        herramientas: [
+            { nombre: "Base de Datos", herramienta: "MySQL 8.0.38" },
+            { nombre: "IDE", herramienta: "Apache NetBeans IDE 24" },
+            { nombre: "Servidor", herramienta: "Apache TomCat 10.1.13" },
+            { nombre: "Cliente API", herramienta: "Postman v.11.31.4" },
+            { nombre: "IDE Alternativa", herramienta: "IntelliJIDEA 2024.2.5" }
+        ],
+        resultados: "API REST para gestionar datos en una base de datos."
+    },
+    {
+        nombre: "Sistema DAQ para Monitoreo de Sensores",
+        objetivo: "Implementar un sistema que interactúe con sensores de temperatura y humedad, almacenando datos en una base de datos mediante una interfaz gráfica.",
+        img: "/img/appmedik.png",
+        herramientas: [
+            { nombre: "Microcontrolador", herramienta: "Arduino Uno" },
+            { nombre: "IDE", herramienta: "Arduino IDE 2.3.4" },
+            { nombre: "IDE", herramienta: "Apache NetBeans IDE 24" },
+            { nombre: "Servicio IoT", herramienta: "Dweet.io" }
+        ],
+        resultados: "Monitoreo en tiempo real con recolección de datos."
+    },
+    {
+        nombre: "Formulario de Usuarios en Android (CRUD)",
+        objetivo: "Implementar un sistema CRUD para gestionar usuarios en dispositivos móviles.",
+        img: "/img/appmedik.png",
+        herramientas: [
+            { nombre: "IDE", herramienta: "Android Studio 2023.1.13" },
+            { nombre: "Base de Datos", herramienta: "SQLite 2.4.0" }
+        ],
+        resultados: "Aplicación móvil funcional con base de datos integrada."
+    }, {
+        nombre: "Formulario de Usuarios en Android (CRUD)",
+        objetivo: "Implementar un sistema CRUD para gestionar usuarios en dispositivos móviles.",
+        img: "/img/appmedik.png",
+        herramientas: [
+            { nombre: "IDE", herramienta: "Android Studio 2023.1.13" },
+            { nombre: "Base de Datos", herramienta: "SQLite 2.4.0" }
+        ],
+        resultados: "Aplicación móvil funcional con base de datos integrada."
+    }
 
-        ];
+];
