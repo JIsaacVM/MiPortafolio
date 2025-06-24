2948896f57fa1dd57216988ea4003a514f2796b4
JIsaacVM <jivmzacisaac@gmail.com>
2025-06-24 17:01:31 -0600

    Actualizacion 23/06

---
+++ src/data/herramientas.js
@@ -0,0 +1,36 @@
+export const ruta = "./img/herramientas/";
+export const herramientas = [
+    {
+        titulo: "Sistemas Operativos",
+        tipos: [
+            { nombre: "Sistema Operativo", herramienta: "Windows 10 y 11", img: ruta + "windows.png" },
+            { nombre: "Sistema Operativo", herramienta: "Ubuntu 24.04", img: ruta + "ubuntu.png" }
+        ]
+    }, {
+        titulo: "Programas",
+        tipos: [
+            { nombre: "Diseño UML", herramienta: "Astah UML ", img: ruta + "astah.png" },
+            { nombre: "Edición de Imágenes", herramienta: "Adobe Photoshop ", img: ruta + "adobe_photoshop.png" },
+            { nombre: "Simulación de Redes", herramienta: "Cisco Packet Tracer ", img: ruta + "cisco.png" },
+            { nombre: "IDE", herramienta: "Apache NetBeans IDE ", img: ruta + "netbeans.png" },
+            { nombre: "Gestión de Bases de Datos", herramienta: "MySQL Workbench ", img: ruta + "mysql.png" },
+            { nombre: "Servidor Web", herramienta: "Apache TomCat ", img: ruta + "tomcat.png" },
+            { nombre: "IDE", herramienta: "IntelliJIDEA", img: ruta + "intellij.png" },
+            { nombre: "Diseño de Interfaces", herramienta: "SceneBuilder", img: ruta + "sceneBuilder.png" },
+            { nombre: "Base de Datos NoSQL", herramienta: "MongoDB Compass", img: ruta + "mongodb.png" },
+            { nombre: "IDE", herramienta: "Android Studio", img: ruta + "android_studio.png" }
+        ]
+    }, {
+        titulo: "Lenguajes de Programación",
+        tipos: [
+            { nombre: "Lenguaje de Programación", herramienta: "Java Development Kit", img: ruta + "java.png" },
+            { nombre: "Lenguaje de Programación", herramienta: "Python", img: ruta + "python.png" },
+            { nombre: "Lenguaje de Marcado", herramienta: "HTML5 ", img: ruta + "html.png" },
+            { nombre: "Lenguaje de Estilo", herramienta: "CSS5 ", img: ruta + "css.png" },
+            { nombre: "Lenguaje de Programación", herramienta: "JavaScript ECMAScript 2020", img: ruta + "javascript.png" },
+            { nombre: "Gestor de Base de Datos", herramienta: "MySQL ", img: ruta + "mysql.png" },
+            { nombre: "Framework", herramienta: "JavaFX ", img: ruta + "javafx.png" },
+            { nombre: "Terminal de Sistema", herramienta: "Linux CLI", img: ruta + "linux.png" }
+        ]
+    }
+];
